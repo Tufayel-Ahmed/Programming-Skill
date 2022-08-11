ffb05f4d257fe23d12c7e1bd07f27414e7cbff0b
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-11 10:57:22 +0600

    1480 Problem Commit

---
+++ _1480/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1491
+{
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + nums[i - 1];
+            }
+            return nums;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] nums = { 1, 2, 3, 4, 5, 6 };
+
+            int[] result = solution.RunningSum(nums);
+
+            Array.ForEach(result, Console.WriteLine);
+        }
+    }
+}
