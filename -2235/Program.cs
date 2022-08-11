9335aaf7b0a033208dbb9f6f085a642d1ea12251
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-11 11:27:23 +0600

    2235 Problem Commit

---
+++ -2235/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2235
+{
+    public class Solution
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Solution solution = new Solution();
+
+            int result = solution.Sum(10, 20);
+
+            Console.WriteLine(result);
+        }
+    }
+}
