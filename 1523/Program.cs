246f3edea4be5a485eb7cc53fa71bf92f22176b7
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-11 10:22:52 +0600

    1523 Problem Commit

---
+++ 1523/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CountOddNumbersInAnIntervalRange
+{
+    public class Solution
+    {
+        public int CountOdds(int low, int high)
+        {
+            if (low % 2 == 0 && high % 2 == 0)
+            {
+                return (high - low) / 2;
+            }
+            else
+            {
+                return ((high - low) / 2) + 1;
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Solution solution = new Solution();
+            int result = solution.CountOdds(1, 100);
+            Console.WriteLine(result);
+        }
+    }
+}
