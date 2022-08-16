aa8c26e480a0291eb8c56a95162151f0249cf503
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-16 17:05:19 +0600

    383 Problem Commit

---
+++ _383/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _383
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Solution solution = new Solution();
+            bool result = solution.CanConstruct("aa", "aab");
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Solution
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+
+            if (ransomNote.Length > magazine.Length)
+            {
+                return false;
+            }
+            foreach (char c in magazine)
+            {
+                int index = ransomNote.IndexOf(c);
+
+                if (index == -1)
+                {
+                    continue;
+                }
+                ransomNote = ransomNote.Remove(index, 1);
+
+                if (ransomNote.Length == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
