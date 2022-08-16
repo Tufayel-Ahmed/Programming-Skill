d47d00ba87fdcd55ff137060b76615ade2815a8f
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-16 16:55:08 +0600

    1342 Problem Commit

--- _1342/Program.cs
+++ _1342/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace _1342
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Solution solution = new Solution();
+            int result = solution.NumberOfSteps(10);
+            Console.WriteLine(result);
+        }
+    }
+    public class Solution
+    {
+        public int NumberOfSteps(int num)
+        {
+            int count = 0;
+            while (num != 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                }
+                else
+                {
+                    num = num - 1;
+                }
+                count++;
+            }
+            return count;
+        }
+    }
+}
