6b8f5309779e31d399dc4338e3c1467a2a637f62
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-26 19:05:50 +0600

    Subtract the Product and Sum of Digits of an Integer Problem Commit

---
+++ SubtracttheProductandSumofDigitsofanInteger/SubtracttheProductandSumofDigitsofanInteger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SubtracttheProductandSumofDigitsofanInteger
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int n = 20;
+
+            Solution solution = new Solution();
+            int result = solution.SubtractProductAndSum(n);
+            Console.WriteLine(result);
+        }
+    }
+    public class Solution
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            int sum = 0, product = 1;
+            while (n != 0)
+            {
+                sum += (n % 10);
+                product *= (n % 10);
+                n = n / 10;
+            }
+            return (product - sum);
+        }
+    }
+}
