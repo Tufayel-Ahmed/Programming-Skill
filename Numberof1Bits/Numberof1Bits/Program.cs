eb311daef24e1a91ed2d4ca4a3c0f7915c460dd5
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-26 19:00:24 +0600

    Number of 1 Bits Problem Commit

---
+++ Numberof1Bits/Numberof1Bits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Numberof1Bits
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            uint n = 1010101001;
+
+            Solution solution = new Solution();
+            int result = solution.HammingWeight(n);
+            Console.WriteLine(result);
+        }
+    }
+    public class Solution
+    {
+        public int HammingWeight(uint n)
+        {
+            int count = 0;
+            while (n > 0)
+            {
+                n &= (n - 1);
+                count++;
+            }
+            return count;
+        }
+    }
+}
