97c54116cb957fc223b5bbfc1c5ae35014b4883a
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-08-27 23:45:04 +0600

    Largest Perimeter Triangle Problem Commit

---
+++ LargestPerimeterTriangle/LargestPerimeterTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LargestPerimeterTriangle
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8,9};
+
+            Solution solution = new Solution();
+            int result = solution.LargestPerimeter(nums);
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Solution
+    {
+        public int LargestPerimeter(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = nums.Length - 3; i >= 0; --i)
+            {
+                if ((nums[i] + nums[i + 1]) > nums[i + 2])
+                {
+                    return nums[i] + nums[i + 1] + nums[i + 2];
+                }
+            }
+            return 0;
+        }
+    }
+}
