1ed41a8b1ab5b7599b523910ce45983c28d5669b
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-11-09 21:17:44 +0600

    Find All Sub Strings of a Given String

---
+++ Find All Substrings of a Given String/Find All Substrings of a Given String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Find_All_Substrings_of_a_Given_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindSubstring("ABC");
+        }
+
+        public static void FindSubstring(string str)
+        {
+            Console.Write("Substrings: ");
+            for(int i = 0; i < str.Length; i++)
+            {
+                StringBuilder subString = new StringBuilder(str.Length - i);
+                for(int j = i; j < str.Length; j++)
+                {
+                    subString.Append(str[j]);
+                    Console.Write($"{subString} ");
+                }
+            }
+        }
+    }
+}
