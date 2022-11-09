bbfb3fc097003d1891bfb665bcb3a471fe96b87d
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-11-09 14:58:45 +0600

    Factorial of a Number

---
+++ Factorial of a Number/Factorial of a Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Factorial_of_a_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = FindFactorial(4);
+            Console.WriteLine($"Factorial is: {result}");
+        }
+
+        public static int FindFactorial(int number)
+        {
+            if(number == 0)
+            {
+                return 1;
+            }
+            return FindFactorial(number - 1) * number;
+        }
+    }
+}
