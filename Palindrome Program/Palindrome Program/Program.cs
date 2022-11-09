67c644e252f123c227190aa3ee839d64c89374cc
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-11-09 20:23:07 +0600

    Palindrome Number

---
+++ Palindrome Program/Palindrome Program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Palindrome_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckPalindrome(555);
+        }
+
+        public static void CheckPalindrome(int number)
+        {
+            int num = number;
+            int reminder;
+            int reverseNumber = 0;
+            for(int i = 0; i < number; i++)
+            {
+                reminder = number % 10;
+                reverseNumber = (reverseNumber * 10) + reminder;
+                number = number / 10;
+            }
+
+            if(num == reverseNumber)
+            {
+                Console.WriteLine("Number is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Number is not palindrome");
+            }
+        }
+    }
+}
