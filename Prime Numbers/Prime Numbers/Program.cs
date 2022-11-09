23d85ac6b6e25c8eb2becbeb218c4548e37749b6
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-11-09 20:08:21 +0600

    Prime Numbers

---
+++ Prime Numbers/Prime Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Prime_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindPrimeNumber(2, 100);
+        }
+
+        public static void FindPrimeNumber(int startNumber, int endNumber)
+        {
+            Console.Write("Prime Number: ");
+            for(int i = startNumber; i < endNumber; i++)
+            {
+                bool isPrime = true;
+                for(int j = 2; j <= i / 2; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if(isPrime)
+                {
+                    Console.Write($"{i}, ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
