342a04aa157ac2aac1d8f36c0aee7487ce7b1efb
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-11-09 21:06:42 +0600

    Remove Duplicate Characters From a String

---
+++ Remove Duplicate Characters From a String/Remove Duplicate Characters From a String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Remove_Duplicate_Characters_From_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoveDuplicate("Hello World!");
+        }
+
+        public static void RemoveDuplicate(string str)
+        {
+            string resultString = String.Empty;
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (!resultString.Contains(str[i]))
+                {
+                    resultString = resultString + str[i];
+                }
+            }
+            Console.WriteLine(resultString);
+        }
+    }
+}
