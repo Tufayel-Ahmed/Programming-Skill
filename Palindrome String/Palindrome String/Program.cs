56b87369203f84018973f270094711997ea9d18b
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-11-09 20:28:15 +0600

    Palindrome String

---
+++ Palindrome String/Palindrome String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Palindrome_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindPalindromeString("madam");
+        }
+
+        public static void FindPalindromeString(string str)
+        {
+            string reverseString = String.Empty;
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                reverseString = reverseString + str[i];
+            }
+            if(str == reverseString)
+            {
+                Console.WriteLine("String is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("String is not Palindrome");
+            }
+        }
+    }
+}
