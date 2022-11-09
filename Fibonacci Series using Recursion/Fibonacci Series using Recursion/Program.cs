ff2060098378dfd53f35e6ccbd62dc7b6764c52c
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-11-09 15:12:54 +0600

    Fibonacci Series using Recursion

---
+++ Fibonacci Series using Recursion/Fibonacci Series using Recursion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fibonacci_Series_using_Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Fibonacci Series: ");
+            FindFibonacciSeries(0, 1, 1, 10);
+        }
+
+        public static void FindFibonacciSeries(int firstNumber, int secondNumber, int counter, int number)
+        {
+            Console.Write($"{firstNumber}, ");
+            if(counter < number)
+            {
+                FindFibonacciSeries(secondNumber, firstNumber + secondNumber, counter + 1, number);
+            }
+        }
+    }
+}
