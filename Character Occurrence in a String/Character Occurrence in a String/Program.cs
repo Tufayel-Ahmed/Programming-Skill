518a70cf4808ddfc74c3e87848296f7306f47dec
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-11-09 20:51:24 +0600

    Character Occurrence in a String

---
+++ Character Occurrence in a String/Character Occurrence in a String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Character_Occurrence_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindOccurrence("Hello World!");
+        }
+
+        public static void FindOccurrence(string str)
+        {
+            str = str.Replace(" ", string.Empty);
+            while(str.Length > 0)
+            {
+                Console.Write($"{str[0]}: ");
+                int count = 0;
+                for(int j = 0; j < str.Length; j++)
+                {
+                    if (str[0] == str[j])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                str = str.Replace(str[0].ToString(), string.Empty);
+            }
+        }
+    }
+}
