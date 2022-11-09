26590c664b54d6e1e7d982ba6b7a1f3da8149572
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-11-09 14:54:48 +0600

    Sum of First N Natural Number

---
+++ Sum of First N Natural Number/Sum of First N Natural Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sum_of_First_N_Natural_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Sum(4);
+            Console.WriteLine($"Sum = {result}");
+        }
+
+        public static int Sum(int number)
+        {
+            if(number <= 0)
+            {
+                return 0;
+            }
+            return Sum(number - 1) + number;
+        }
+    }
+}
